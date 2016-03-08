93a716dc66f863d5775e882ed0ea4c3666374be9
8c6794b6 <8c6794b6@gmail.com>
2016-03-08 19:36:45 +0900

    Added div from Gabriel benchmark as test
    * test-suite/tjit/Makefile.am: Added new test.
    
    * test-suite/tjit/x-div.scm: New file.

---
+++ test-suite/tjit/x-div.scm
@@ -0,0 +1,44 @@
+;;; `diviter' and `divrec' from Gabriel benchmark.
+
+(define (create-n n)
+  (do ((n n (- n 1))
+       (a '() (cons '() a)))
+      ((= n 0) a)))
+
+(define *ll* (create-n 200))
+
+(define (iterative-div2 l)
+  (do ((l l (cddr l))
+       (a '() (cons (car l) a)))
+      ((null? l) a)))
+
+(define (recursive-div2 l)
+  (cond ((null? l) '())
+        (else (cons (car l) (recursive-div2 (cddr l))))))
+
+(define (test-1 l)
+  (do ((i 3000 (- i 1)))
+      ((= i 0))
+    (iterative-div2 l)
+    (iterative-div2 l)
+    (iterative-div2 l)
+    (iterative-div2 l)))
+
+(define (test-2 l)
+  (do ((i 3000 (- i 1)))
+      ((= i 0))
+    (recursive-div2 l)
+    (recursive-div2 l)
+    (recursive-div2 l)
+    (recursive-div2 l)))
+
+(define (run)
+  (let loop ((n 15) (v 0))
+    (if (zero? n)
+        v
+        (loop (- n 1)
+              (cons
+               (test-1 *ll*)
+               (test-2 *ll*))))))
+
+(run)
