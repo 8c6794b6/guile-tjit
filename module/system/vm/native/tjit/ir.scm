e08c24b078614171410589c23e92138b4901dbf9
8c6794b6 <8c6794b6@gmail.com>
2016-04-26 22:58:21 +0900

    Decrease failure increment for too many inline
    * module/system/vm/native/tjit/ir.scm (current-inline-depth): Modified
      failure increment count for too many inlined procedures.

--- module/system/vm/native/tjit/ir.scm
+++ module/system/vm/native/tjit/ir.scm
@@ -226,7 +226,7 @@ returns, current call-num, and current return-num."
              (lp calls acc)))
         (()
          (when (< (tjit-max-inline-depth) acc)
-           (break 5 "too many inlined procedures"))
+           (break 2 "too many inlined procedures"))
          (+ acc (env-inline-depth env)))))))
 
 (define* (take-snapshot ip dst-offset locals vars indices id sp-offset fp-offset
